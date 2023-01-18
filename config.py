0352b0516d2c5a53f1314080c9c1e06701333001
mlbtheroku <115041575+mlbtheroku@users.noreply.github.com>
2023-01-18 11:07:00 +0630

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -4,20 +4,20 @@ class Config(object):
     # get a token from https://chatbase.com
     # CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5945346287:AAE3gmyeGs-oK2UxcgbTDWjr48an4iv-oSY")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID", 22681384))
+    API_HASH = os.environ.get("API_HASH", "14ae45755537c723aab0564a80d723a9")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5468192421").split())
     # Banned Unwanted Members..
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Update channel for Force Subscribe
-    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
-    AUTH_CHANNEL = "@myTestbotz"
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "-1001523739263")
+    AUTH_CHANNEL = "@h2dts"
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
     TG_MAX_FILE_SIZE = 2097152000
@@ -39,10 +39,10 @@ class Config(object):
     DEF_WATER_MARK_FILE = ""
     
     #Log channel for banning spammers
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001860694129"))
     # Time Limit
     try:
-        TIME_GAP = int(os.environ.get("TIME_GAP", "")) if os.environ.get("TIME_GAP", "") else None
+        TIME_GAP = int(os.environ.get("TIME_GAP", "120")) if os.environ.get("TIME_GAP", "") else None
     except:
         TIME_GAP = None
         logger.warning("Give the timegap in seconds. Dont use letters 😑")
