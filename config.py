ec14e3c3ac3d1e7c761820f1a9ddfce94f2a39cf
mlbtheroku <115041575+mlbtheroku@users.noreply.github.com>
2023-01-18 11:12:37 +0630

    Create config.py

---
+++ config.py
@@ -0,0 +1,50 @@
+import os
+
+class Config(object):
+    # get a token from https://chatbase.com
+    # CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
+    # get a token from @BotFather
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5945346287:AAE3gmyeGs-oK2UxcgbTDWjr48an4iv-oSY")
+    # The Telegram API things
+    APP_ID = int(os.environ.get("APP_ID", 22681384))
+    API_HASH = os.environ.get("API_HASH", "14ae45755537c723aab0564a80d723a9")
+    # Get these values from my.telegram.org
+    # Array to store users who are authorized to use the bot
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5468192421").split())
+    # Banned Unwanted Members..
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    # Update channel for Force Subscribe
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "-1001523739263")
+    AUTH_CHANNEL = "@h2dts"
+    # Telegram maximum file upload size
+    MAX_FILE_SIZE = 50000000
+    TG_MAX_FILE_SIZE = 2097152000
+    FREE_USER_MAX_FILE_SIZE = 50000000
+    # chunk size that should be used with requests
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    # default thumbnail to be used in the videos
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    # proxy for accessing youtube-dl in GeoRestricted Areas
+    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    # https://t.me/hevcbay/951
+    OUO_IO_API_KEY = ""
+    # maximum message length in Telegram
+    MAX_MESSAGE_LENGTH = 4096
+    # set timeout for subprocess
+    PROCESS_MAX_TIMEOUT = 3600
+    # watermark file
+    DEF_WATER_MARK_FILE = ""
+    
+    #Log channel for banning spammers
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001860694129"))
+    # Time Limit
+    try:
+        TIME_GAP = int(os.environ.get("TIME_GAP", "120")) if os.environ.get("TIME_GAP", "") else None
+    except:
+        TIME_GAP = None
+        logger.warning("Give the timegap in seconds. Dont use letters 😑")
+    TIME_GAP_STORE = {}
+
